f071ae18bcb86a7ccb0c9d0a01860672361776fa
rizkyhidayattulloh <rizkynear1@gmail.com>
2023-10-02 13:25:18 +0800

    fix bootstrap styling

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import "./scss/main.scss";
 import DefaultTheme from "vitepress/theme";
 import Baskito from "@timedoor/baskito-ui";
 
+import "./scss/custom-bootstrap.scss";
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
